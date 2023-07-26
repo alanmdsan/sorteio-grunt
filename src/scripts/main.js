10849f1983bff93ebde7052faf644e0357038755
alanmdsan <alanmdsan@gmail.com>
2023-07-26 20:50:08 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('form-sorteio').addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        let numero_maximo = document.getElementById('numero-maximo').value;
+        numero_maximo = parseInt(numero_maximo);
+
+        let numero_aleatorio = Math.random() * numero_maximo;
+        numero_aleatorio = Math.floor(numero_aleatorio + 1);
+
+        document.getElementById('resultado').innerText = numero_aleatorio;
+        document.querySelector('.resultado').style.display = 'block';
+    });
+})
